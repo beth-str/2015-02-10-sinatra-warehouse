884819c515d56814c6eb350057c5c1fd45e9d657
Beth Stranz <bethstranz@hotmail.com>
2015-02-11 16:10:27 -0600

    product add / product delete functional

--- menu.rb
+++ menu.rb
@@ -1,17 +1,9 @@
-require "sinatra"
-# require 'activesupport'
+require 'sinatra'
 require 'sqlite3'
-# DATABASE = SQLite3::Database.new("warehouse.db")
 require_relative "2015-02-05-warehouse/category"
 require_relative "2015-02-05-warehouse/db_setup"
 require_relative "2015-02-05-warehouse/location"
 require_relative "2015-02-05-warehouse/product"
-
-
-# require_relative "module"
-# include 'WarehouseHelper'
-
-
 
 get "/" do
   erb :homepage, :layout => :boilerplate
@@ -22,7 +14,8 @@ get "/add_product" do
 end
 
 get "/add_product_confirm" do
-  
+  x = Product.new(params)
+  x.insert
   erb :add_product_confirm, :layout => :boilerplate
 end
 
@@ -30,38 +23,60 @@ get "/edit_product" do
   erb :edit_product, :layout => :boilerplate
 end
 
+get "/edit_product_confirm" do
+  erb :add_product_confirm, :layout => :boilerplate
+end
+
 get "/delete_product" do
   erb :delete_product, :layout => :boilerplate
+end
+
+get "/delete_product_confirm" do
+  Product.delete(params["title"])
+  erb :delete_product_confirm, :layout => :boilerplate
 end
 
 get "/show_product" do
   erb :show_product, :layout => :boilerplate
 end
 
+get "/show_product_confirm" do
+  erb :show_product_confirm, :layout => :boilerplate
+end
+
 get "/add_genre" do
   erb :add_genre, :layout => :boilerplate
+end
+
+get "/add_genre_confirm" do
+  erb :add_genre_confirm, :layout => :boilerplate
 end
 
 get "/delete_genre" do
   erb :delete_genre, :layout => :boilerplate
 end
 
+get "/delete_genre_confirm" do
+  Category.delete(params["id"])
+  erb :delete_genre_confirm, :layout => :boilerplate
+end
+
 get "/add_location" do
   erb :add_location, :layout => :boilerplate
+end
+
+get "/add_location_confirm" do
+  erb :add_location_confirm, :layout => :boilerplate
 end
 
 get "/delete_location" do
   erb :delete_location, :layout => :boilerplate
 end
 
-get "/confirmation" do
-  erb :confirmation, :layout => :boilerplate
-  @title = params[:title]
+get "/delete_location_confirm" do
+  Location.delete(params["id"])
+  erb :delete_location_confirm, :layout => :boilerplate
 end
-
-
-
-
 
 #
 # get "/result" do
